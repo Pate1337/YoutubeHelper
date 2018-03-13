dadf21236958b451e82e2ba0b863154407dcefca
Pate1337 <paavo.hemmo@hotmail.com>
2018-03-13 15:01:31 +0200

    index.js ja package.json konfiguroitu

---
+++ index.js
@@ -0,0 +1,34 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+
+mongoose.connect(config.mongoUrl)
+  .then( () => {
+    console.log('connected to database', config.mongoUrl)
+  })
+  .catch( err => {
+    console.log(err)
+  })
+
+mongoose.Promise = global.Promise
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const server = http.createServer(app)
+
+server.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`)
+})
+
+server.on('close', () => {
+  mongoose.connection.close()
+})
+
+module.exports = {
+  app, server
+}
