4cd838b113f3df50164a83e5d1ffe85a93114101
Pate1337 <paavo.hemmo@hotmail.com>
2018-03-14 15:18:28 +0200

    Käyttäjälle määritellyt linkit saadaan otettua vastaan taulukkona jonka alkioina olioita

--- models/user.js
+++ models/user.js
@@ -13,19 +13,27 @@ const User = [
     id: '123123',
     username: 'testi1',
     name: 'Testi1',
-    links: {
+    links: [{
       id: '19304',
       url: 'https://www.youtube.com/watch?v=TPXWtozVNzM'
-    }
+    },
+    {
+      id: '1337',
+      url: 'https://github.com/Pate1337/YoutubeHelper'
+    }]
   },
   {
     id: '9876986',
     username: 'testi2',
     name: 'Testi2',
-    links: {
+    links: [{
       id: '85740',
       url: 'https://www.youtube.com/watch?v=tsTZ2iFRSmw'
-    }
+    },
+    {
+      id: '836743123',
+      url: 'https://fullstack-hy.github.io/'
+    }]
   }
 ]
 module.exports = User
