1bfac5bec48798192a7a4d1c1f6da70cce78167c
Mikko <mitami@ad.helsinki.fi>
2018-05-20 14:45:30 +0300

    nyt poistettu kommentti poistuu sekä tietokannasta dokumenttina, että vastaanottaja-userin rComments taulukosta

--- testControllers/comments.js
+++ testControllers/comments.js
@@ -19,7 +19,7 @@ commentsRouter.get('/', async (request, response) => {
 
 commentsRouter.get('/:id', async (request, response) => {
   console.log('commentsRouter GET_BY_USER_ID')
-  const comments = await Comment.find({receiver:request.params.id})
+  const comments = await Comment.find({ receiver: request.params.id })
   //console.log(comments)
   response.json(comments.map(Comment.format))
 })
@@ -36,15 +36,19 @@ commentsRouter.post('/', async (request, response) => {
     }
     console.log('commentsRouter TOKEN verifioitu')
     const body = request.body
-    const receiver = await User.findById(body.receiver)
-    const sender = await User.findById(decodedToken.id)
+    
+      const receiver = await User.findById(body.receiver)
+      const sender = await User.findById(decodedToken.id)
+    
     console.log('cRouter decTOKEN _ID', decodedToken.id)
 
     const comment = new Comment({
       content: body.content,
       receiver: body.receiver,
-      sender: { id: body.sender,
-                name: sender.username}
+      sender: {
+        id: body.sender,
+        name: sender.username
+      }
     })
     console.log('commentsRouter KOMMENTTI luotu')
     const savedComment = await comment.save()
@@ -54,10 +58,16 @@ commentsRouter.post('/', async (request, response) => {
     sender.sentComments = sender.sentComments.concat(savedComment._id)
     console.log('kommentti tallennettu lähettäjälle')
 
+    if (sender.id != receiver.id) {
+      await sender.save()
+      console.log('lähettäjä user tallennettu')
+    } else {
+      receiver.sentComments = receiver.sentComments.concat(savedComment._id)
+    }
     await receiver.save()
     console.log('saaja user tallennettu')
-    await sender.save()
-    console.log('lähettäjä user tallennettu')
+    
+
 
     return response.status(201).json(savedComment)
 
@@ -83,15 +93,19 @@ commentsRouter.delete('/:id', async (request, response) => {
       return response.status(401).json({ error: 'token missing or invalid' })
     }
     const commentToDelete = await Comment.findById(request.params.id)
-    console.log('DecodedTokenID',decodedToken.id)
+    let userToDeleteFrom = await User.findById(decodedToken.id)
+    //console.log('USER LÖYDETTY commentsController', userToDeleteFrom)
+    userToDeleteFrom.rComments = userToDeleteFrom.rComments.filter(rcomment => rcomment != request.params.id)
+    await userToDeleteFrom.save()
+    console.log('DecodedTokenID', decodedToken.id)
     console.log('RECEIVER', commentToDelete.receiver)
-    if(decodedToken.id == commentToDelete.receiver) {
+    if (decodedToken.id == commentToDelete.receiver) {
       console.log('token was same as parameter id')
       await Comment.findByIdAndRemove(request.params.id)
       return response.status(204).end()
     } else {
       console.log('token was not the same as parameter id')
-      return response.status(401).json({ error: 'No authorization'})
+      return response.status(401).json({ error: 'No authorization' })
     }
 
   } catch (e) {
